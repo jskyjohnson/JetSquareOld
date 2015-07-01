73c8f2492c12060b7fff48da135f85fab68f7f20
AndyY <ayan9005@bvsd.org>
2015-06-30 22:19:27 -0600

    Re-organized

---
+++ Assets/Scripts/DeathBlock.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathBlock : MonoBehaviour {
+	float a = 0F;
+	public Color currentColor;
+	// Use this for initialization
+	void Start () {
+	}
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Assets/Scripts/Mobile.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mobile : MonoBehaviour {
+
+	// Use this for initialization
+	public float orthographicSize = 5;
+	public float aspect = 1.33333f;
+
+	void Start()
+	{
+		Camera.main.projectionMatrix = Matrix4x4.Ortho(
+			-orthographicSize * aspect, orthographicSize * aspect,
+			-orthographicSize, orthographicSize,
+			GetComponent<Camera>().nearClipPlane, GetComponent<Camera>().farClipPlane);
+	}
+}
