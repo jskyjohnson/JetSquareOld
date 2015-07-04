796f2eec57d49b4752ccee4b0b5305c05eafa5c4
AndyY <ayan9005@bvsd.org>
2015-07-04 14:46:15 -0600

    Change player model, player shadow.

--- Assets/Scripts/PlayerShadow.cs
+++ Assets/Scripts/PlayerShadow.cs
@@ -2,11 +2,12 @@
 using System.Collections;
 
 public class PlayerShadow : MonoBehaviour {
-
+	public GameObject playershadow;
 	// Use this for initialization
 	void Start () {
+		playershadow.GetComponent<Rigidbody2D>().velocity = new Vector3 (UnityEngine.Random.Range (-5.0f, 5.0f), UnityEngine.Random.Range (-5.0f, 5.0f), UnityEngine.Random.Range (-5.0f, 5.0f));
 		if (this.name == "playershadow(Clone)") {
-			Destroy (this.gameObject, 0.08f);
+			Destroy (this.gameObject, 0.3f);
 		}
 	}
 	// Update is called once per frame
