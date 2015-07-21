50ca8a8297be7857e90fdedf4bb543941abfd159
AndyY <ayan9005@bvsd.org>
2015-07-20 21:23:42 -0600

    Added buttons for in-app purchases

---
+++ Assets/Scripts/AppAssets.cs
@@ -0,0 +1,82 @@
+﻿/// Copyright (C) 2012-2014 Soomla Inc.
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///      http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Soomla;
+using Soomla.Store;
+	
+	/// <summary>
+	/// This class defines our game's economy, which includes virtual goods, virtual currencies
+	/// and currency packs, virtual categories
+	/// </summary>
+	public class AppAssets : IStoreAssets{
+		
+		/// <summary>
+		/// see parent.
+		/// </summary>
+		public int GetVersion() {
+			return 0;
+		}
+		
+		/// <summary>
+		/// see parent.
+		/// </summary>
+		public VirtualCurrency[] GetCurrencies() {
+			return new VirtualCurrency[]{};
+		}
+		
+		/// <summary>
+		/// see parent.
+		/// </summary>
+		public VirtualGood[] GetGoods() {
+			return new VirtualGood[] {COINS_200_ITEM, NO_ADS_LTVG};
+		}
+		
+		/// <summary>
+		/// see parent.
+		/// </summary>
+		public VirtualCurrencyPack[] GetCurrencyPacks() {
+			return new VirtualCurrencyPack[] {};
+		}
+		
+		/// <summary>
+		/// see parent.
+		/// </summary>
+		public VirtualCategory[] GetCategories() {
+			return new VirtualCategory[]{};
+		}
+		
+		/** Static Final Members **/
+		
+		public const string COINS_200_ID = "coins200";
+
+		public const string NO_ADS_LIFETIME_PRODUCT_ID = "android.test.purchased";//"no_ads";
+		
+		public static VirtualGood COINS_200_ITEM = new SingleUseVG(
+			"200 Coins",                                       		// name
+			"Get 200 Coins", // description
+			"coins200",                                       		// item id
+			new PurchaseWithMarket(new MarketItem(COINS_200_ID, 0.99))); // the way this virtual good is purchased
+
+		/** LifeTimeVGs **/
+		// Note: create non-consumable items using LifeTimeVG with PuchaseType of PurchaseWithMarket
+		public static LifetimeVG NO_ADS_LTVG = new LifetimeVG(
+			"No Ads", 														// name
+			"No More Ads!",				 									// description
+			"no_ads",														// item id
+			new PurchaseWithMarket(new MarketItem(NO_ADS_LIFETIME_PRODUCT_ID, 0.99)));	// the way this virtual good is purchased
+	}
+	
--- Assets/Scripts/ShopScript.cs
+++ Assets/Scripts/ShopScript.cs
@@ -3,64 +3,90 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 using System;
-public class ShopScript : MonoBehaviour {
-	public Canvas ShopMenu;
-	public static Text coins;
-	public Button backButton;
-	int coinsvalue;
-	public static string[] currentskins = {};
-	public Button[] skinsButtons;
-	public Sprite unknownImage;
-	// Use this for initialization
-	void Start () {
-		ShopMenu = ShopMenu.GetComponent<Canvas> ();
-		currentskins = PlayerPrefsX.GetStringArray ("purchased");
-		coins = ShopMenu.GetComponentInChildren<Text> ();
-		loadcoinvalue (PlayerPrefs.GetInt ("coins"));
-		resetSkinsButtons ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+using Soomla;
+using Soomla.Store;
 
-	public void back() {
-		Application.LoadLevel ("Menu");
-	}
 
-	public static void loadcoinvalue(int coinsvalue) {
-		coins.text = "Total Coins: " + coinsvalue;
-	}
-	public void resetSkinsButtons() {
-		foreach (Button button in skinsButtons) {
-			currentskins = ShopScript.currentskins;
-			bool contains = false;
-			//checks if already purchased
-			for (int i = 0; i < currentskins.Length; i++) {
-				if(currentskins[i] == button.name) {
-					contains = true;
+	public class ShopScript : MonoBehaviour {
+		public Canvas ShopMenu;
+		public static Text coins;
+		public Button backButton;
+		int coinsvalue;
+		public static string[] currentskins = {};
+		public Button[] skinsButtons;
+		public Sprite unknownImage;
+
+		// Use this for initialization
+		void Start () {
+			ShopMenu = ShopMenu.GetComponent<Canvas> ();
+			currentskins = PlayerPrefsX.GetStringArray ("purchased");
+			coins = ShopMenu.GetComponentInChildren<Text> ();
+			loadcoinvalue (PlayerPrefs.GetInt ("coins"));
+			resetSkinsButtons ();
+
+			//StoreEvents.OnSoomlaStoreInitialized += onSoomlaStoreInitialized;
+			SoomlaStore.Initialize (new AppAssets ());
+			//StoreEvents.OnItemPurchaseStarted += (PurchasableVirtualItem pvi, string str, Dictionary<string, string> dic) => {};
+			StoreEvents.OnItemPurchased += (PurchasableVirtualItem pvi, string payload) => { 
+				switch(pvi.ID) {
+				case AppAssets.COINS_200_ID:
+					PlayerPrefs.SetInt ("coins", PlayerPrefs.GetInt ("coins") + 200);
+					ShopScript.loadcoinvalue (PlayerPrefs.GetInt ("coins"));
+					break;
+				case AppAssets.NO_ADS_LIFETIME_PRODUCT_ID:
+				Debug.Log ("THIS HSOULD HSOW UP!!!!!");
+					break;
+				}
+			};
+		}
+		public void back() {
+			Application.LoadLevel ("Menu");
+		}
+
+		public static void loadcoinvalue(int coinsvalue) {
+			coins.text = "Total Coins: " + coinsvalue;
+		}
+		public void resetSkinsButtons() {
+			foreach (Button button in skinsButtons) {
+				currentskins = ShopScript.currentskins;
+				bool contains = false;
+				//checks if already purchased
+				for (int i = 0; i < currentskins.Length; i++) {
+					if(currentskins[i] == button.name) {
+						contains = true;
+					}
+				}
+				
+				//handle Color when items are loaded.
+				Image itemImage = button.image;
+				if (PlayerPrefs.GetString ("currentskin") == button.name) {
+					//if item is currently used
+					itemImage.color = new Color (0.34509f, 0.94509f, 1f);
+					Debug.Log (button.name);
+				} else if (!(PlayerPrefs.GetString ("currentskin") == button.name) && contains) {
+					//if item is bought but not currently used
+					itemImage.color = new Color (1f, 1f, 1f);
+					Debug.Log (button.name);
+				} else {
+					//set to unknown picture
+					Debug.Log ("setting to unknown");
+					button.image.overrideSprite = unknownImage;
+					Debug.Log (button.name);
+				}
+				//unknown does not have a configuration because item will stay black despite color because black things tinted different colors are still black.
+				button.image = itemImage;
+			}
+		}
+		public void buy(String name) {
+		Debug.Log ("called");
+			foreach (VirtualGood vg in StoreInfo.Goods) {
+				if(vg.Name == name) {
+					try {
+						StoreInventory.BuyItem (vg.ItemId);
+					} catch (Exception e) {
+						Debug.Log ("SOOMLA" + e.Message);
+					}
 				}
 			}
-			
-			//handle Color when items are loaded.
-			Image itemImage = button.image;
-			if (PlayerPrefs.GetString ("currentskin") == button.name) {
-				//if item is currently used
-				itemImage.color = new Color (0.34509f, 0.94509f, 1f);
-				Debug.Log (button.name);
-			} else if (!(PlayerPrefs.GetString ("currentskin") == button.name) && contains) {
-				//if item is bought but not currently used
-				itemImage.color = new Color (1f, 1f, 1f);
-				Debug.Log (button.name);
-			} else {
-				//set to unknown picture
-				Debug.Log ("setting to unknown");
-				button.image.overrideSprite = unknownImage;
-				Debug.Log (button.name);
-			}
-			//unknown does not have a configuration because item will stay black despite color because black things tinted different colors are still black.
-			button.image = itemImage;
 		}
 	}
-}
